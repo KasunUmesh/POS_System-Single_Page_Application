c8162903cb094084ee2192304b4b30b666ee9c45
KasunUmesh <kasunumesh38@gmail.com>
2023-06-26 18:55:48 +0530

    Created Item code load Selector

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,10 +1,13 @@
 import { getCustomerDB } from "../db/CustomerDB.js";
+import { getItemDB } from "../db/ItemDB.js";
 
 export class OrderController {
   constructor() {
     $("#customerID").click(this.loadCustomerDetails.bind(this));
+    $("#itemCode").click(this.loadItemDetails.bind(this));
 
     this.loadCustomers();
+    this.loadItems();
   }
 
   loadCustomers() {
@@ -29,6 +32,20 @@ export class OrderController {
     $("#cusSalary").val(selectedCustomer._customer_salary);
     $("#cusAddress").val(selectedCustomer._customer_address);
   }
+
+  loadItems() {
+    let items = getItemDB();
+    let itemCode = $("#itemCode");
+    itemCode.empty();
+    itemCode.append($("<option selected>").text("Select Item Code"));
+    items.forEach((items) => {
+      itemCode.append(
+        $("<option>").val(JSON.stringify(items)).text(items._item_code)
+      );
+    });
+  }
+
+  loadItemDetails() {}
 }
 
 new OrderController();
